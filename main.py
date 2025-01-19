b2f8a1359b6a27d18ab28184d3a7bf3c68f05945
SparkingSquirrel <sparkingsquirrel@gmail.com>
2025-01-19 09:18:38 -0600

    update and draw by groups

--- main.py
+++ main.py
@@ -15,19 +15,25 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-
-
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
-        player.update(dt)
+        for thing in updatable:
+            thing.update(dt)
+
         screen.fill((0, 0, 0)) 
-        player.draw(screen)
+        for thing in drawable:
+            thing.draw(screen)
         pygame.display.flip()
+        
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
